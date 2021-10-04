c6ed73211330f3637d54533bab55c8229bc0b11c
Jaromir Roth <jaromirroth@gmail.com>
2021-10-05 00:20:32 +0200

    Fix tailwind purge files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   mode: 'jit',
   purge: {
     enabled: true,
-    content: ['./src/**/*.{html,ts}']
+    content: ['./src/**/*.{html,ts,svg}']
   },
   darkMode: false,
   theme: {
